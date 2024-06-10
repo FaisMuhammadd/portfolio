51434e9b5927ddb27198cc84f4b489bf174b6021
pandawaa <pallakangoding@gmail.com>
2024-06-10 15:59:43 +0800

    added framer motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
-import aboutImg from "../assets/about.jpg";
+import aboutImg from "../assets/about2.jpg";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -8,16 +9,31 @@ const About = () => {
         About <span>Me</span>
       </h2>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
-            <img src={aboutImg} alt="about" className="rounded-2xl" />
+            <img
+              src={aboutImg}
+              alt="about"
+              className="rounded-2xl"
+              width={600}
+            />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
