315373e30714d6c206000ca9a879da31acdfa0e8
pandawaa <pallakangoding@gmail.com>
2024-06-10 16:03:11 +0800

    modified constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,75 +2,64 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `enthusiastic and motivated web developer with a budding career in web development. With less than a year of hands-on experience, has developed a strong foundation in front-end technologies such as HTML, CSS, and JavaScript, and is beginning to explore back-end development with tools like Node.js and Express.js. successfully completed several personal projects, including a responsive personal portfolio website and a basic blog platform, ability to create user-friendly and visually appealing websites.
+Eager to expand my skill set, continuous learning through online courses, tutorials, and coding challenges, also a proactive participant in coding communities and forums, seeking advice and collaborating with peers 	to enhance my knowledge and expertise. My passion for web development, coupled with a strong commitment to personal and professional growth. With continued learning and experience, I wish to become a highly skilled and valuable member of any web development team
+.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2023",
+    role: "Full Stack Developer Intern",
+    company: "Klinik Inggit Medical Centre.",
+    description: `A Fullstack Developer Internship offers aspiring developers a comprehensive experience in both frontend and backend development, enabling them to build and maintain user interfaces and server-side logic. Interns will work with modern technologies like HTML, CSS, JavaScript, and frameworks such as React on the frontend, as well as languages like Node.js and databases like MySQL, MongoDB on the backend.`,
+    technologies: ["Javascript", "React.js", "Node.js", "mongoDB"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Threads Cllone",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Build a full stack Threads clone using Next.js 14+, user interaction to community management, technical implementation, and various features, including nested deep comments, notifications, real-time-search.",
+    technologies: ["TypeScript", "NextJS", "MongoDB", "TailwindCSS", "Zod"],
   },
   {
-    title: "Task Management App",
+    title: "Brainwave",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles. Its sleek design, seamless animations, and overall user experience set a high standard, serving as a reference or inspiration for future modern applications or websites in general.",
+    technologies: ["ReactJS", "TailwindCSS", "Vite"],
   },
   {
-    title: "Portfolio Website",
+    title: "Metaverse Single Page",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "A web user interface static about the metaverse typically refers to a non-interactive webpage or website that provides information, resources, or discussions related to the concept of the metaverse. These static interfaces often serve as introductory platforms, offering descriptions, articles, graphics, and links to further explore the idea of the metaverse.",
+    technologies: ["ReactJS", "TailwindCSS", "NextJS"],
   },
   {
-    title: "Blogging Platform",
+    title: "Lyrics",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Build a Music Web app With a modern homepage, fully-fledged music player, search, lyrics, song exploration features, search, popular music around you, worldwide top charts.",
+    technologies: ["ReactJS", "RapidAPI", "Vite", "Axios"],
+  },
+  {
+    title: "Dalle AI Image Generator",
+    image: project5,
+    description:
+      "An AI image web generator is an online tool that utilizes artificial intelligence algorithms to create images from user input. These generators can produce a wide range of visuals, from realistic photos to abstract art, based on text descriptions, sketches, or other criteria provided by the user.",
+    technologies: ["ReactJS", "TailwindCSS", "ExpressJS", "MongoDB"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Makassar, Indonesia",
+  phoneNo: "+62 895-8034-74380",
+  email: "muhammadfais2525@gmail.com",
 };
